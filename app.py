4771344df90e214abb9437271a7cb37d65ead337
IBRASANN <ibrahim.sanna491@gmail.com>
2026-01-04 16:13:46 +0300

    fixed error in the py code

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert add(-2, -3) == -5
-    assert add(5, 1) == 999
     print("All tests passed!")
 
 def create_report():
